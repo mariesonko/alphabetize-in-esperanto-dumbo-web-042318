047fe6e428a48a4525f437c5a868970ec48928ce
Marie Sonko <mariesonko62@gmail.com>
2018-04-23 02:36:18 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,8 @@
 def alphabetize(arr)
-  arr.sort_by do |word| word.split("").map do |letter| CRAZY_ALPHABET.index(letter)
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |word| word.split("").map do |letter| alphabet.index(letter)
   end
-end 
+end
 end
 #array.sort_by do |word|
   #word.split('').map do |letter|
