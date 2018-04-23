14fb86fec7e16eee687819c9778abff005849620
Marie Sonko <mariesonko62@gmail.com>
2018-04-23 02:31:15 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,7 +1,7 @@
-def alphabetize (arr, rev=false)
-  if rev
-    arr.sort.reverse
-  else
-    arr.sort
-  end
+def alphabetize(arr)
+  arr.sort_by {|word| word.split("").map {|letter| CRAZY_ALPHABET.index(letter)}}
 end
+#array.sort_by do |word|
+  #word.split('').map do |letter|
+  #  CRAZY_ALPHABET.index(letter)
+#  end
